ba4481c36e8a60540754e74b4463609bdca31030
Aleksandr Kuplays <alexkuplays@gmail.com>
2019-05-07 01:19:10 +0700

    migrated from gradle to maven

--- src/main/java/sibguti/repository/MovieRepository.java
+++ src/main/java/sibguti/repository/MovieRepository.java
@@ -3,6 +3,7 @@ package sibguti.repository;
 import org.springframework.data.repository.CrudRepository;
 import sibguti.entity.Movie;
 
+
 public interface MovieRepository extends CrudRepository<Movie, Integer> {
 
 }
